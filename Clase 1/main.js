33c5a871f1afd955509b015a845b086a79ae54e8
andresf2448 <andresf2448@gmail.com>
2023-11-23 18:25:51 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,56 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 20; //variable de tipo dato number
+// let nombre = "andres"; //variable de tipo dato string
+
+//forma no recomendada para declarar variables
+// var numero = 45;
+
+//constantes
+// const NUMERO = 6;
+
+//camelCase => nombreBonitoGrande
+//PascalCase => NombreBonitoGrande
+//snake_case => nombre_bonito_grande
+//kebab-case => nombre-bonito-grande
+
+// let numeroUno = 3;
+// let numeroDos = 15;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("resultado suma", resultadoSuma);
+// console.log("resultado resta", resultadoResta);
+// console.log("resultado multiplicacion", resultadoMultiplicacion);
+// console.log("resultado division", resultadoDivision);
+
+// let numero = 0.35;
+// console.log(typeof numero);
+
+// let nombre = "andres";
+// console.log(typeof nombre);
+
+// let saludo = "Hola";
+// let nombre = "andres";
+
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+
+// let nombre = prompt("Ingrese su nombre");
+// console.log(nombre);
+
+//parseInt("5")
+//parseFloat("5.1234")
+//parseNumber("5")
+let numero = Number(prompt("Ingrese un número"));
+alert(numero + 7);
