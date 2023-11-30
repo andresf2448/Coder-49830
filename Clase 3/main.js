630a84b7e603eb18693e4cc80c6403e9743c8575
andresf2448 <andresf2448@gmail.com>
2023-11-30 18:31:32 -0500

    Clase 3

--- Clase 3/main.js
+++ Clase 3/main.js
@@ -61,10 +61,123 @@ for(desde; hasta; actualizacion){
 //   console.log(i);
 // }
 
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    continue;
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// let saludo = "Hola";
+// let nombre = "andres";
+
+// console.log(saludo + " " + nombre);
+// console.log(`${saludo} ${nombre}`);
+
+/* 
+estructura del bucle while
+
+while(condicion){
+  codigo a repetir siempre que la condicion sea verdadera
+}
+*/
+
+// let repetir = true;
+
+// while (repetir) {
+//   console.log("Hola coder");
+// }
+
+// let entrada = prompt("Ingrese el usuario");
+
+// while (entrada === "pepito") {
+//   alert("usuario incorrecto");
+
+//   entrada = prompt("Ingrese el usuario");
+// }
+
+// alert("Bienvenido");
+
+/* 
+estructura del bucle do while
+
+do{
+  codigo a ejecutar por primera vez y luego siempre que la condicion sea verdadera
+}while(condicion);
+*/
+
+// let repetir = false;
+
+// while (repetir) {
+//   console.log("hola");
+// }
+
+// do {
+//   console.log("Hola");
+// } while (repetir);
+
+// let numero;
+
+// do {
+//   numero = prompt("Ingrese un numero");
+// } while (parseInt(numero));
+
+/* 
+estructura del switch
+
+switch(valor){
+  case valor1:
+    codigo a ejecutar cuando el valor === valor1
+    break;
+
+  case valor2:
+    codigo a ejecutar cuando el valor === valor2
+    break;
+
+  .
+  .
+  .
+
+  default:
+    codigo a ejecutar cuando ninguno de los valores coincide con valor
+    break;
+}
+*/
+
+// let moneda = "usd";
+
+// switch (moneda) {
+//   case "ars":
+//     console.log("Moneda de Argentina");
+//     break;
+
+//   case "cop":
+//     console.log("Moneda de Colombia");
+//     break;
+
+//   case "clp":
+//     console.log("Moneda de chile");
+//     break;
+//   default:
+//     console.log("Moneda no reconocida");
+//     break;
+// }
+
+let entrada = prompt("Ingresar un nombre");
+
+while (entrada != "ESC") {
+  switch (entrada) {
+    case "ANA":
+      alert("HOLA ANA");
+      break;
+    case "JUAN":
+      alert("HOLA JUAN");
+      break;
+    default:
+      alert("¿QUIÉN SOS?");
+      break;
   }
 
-  console.log(i);
+  entrada = prompt("Ingresar un nombre");
 }
