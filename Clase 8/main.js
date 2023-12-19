f7ef772127a4a093b1e7ab39f2692ece15c7498c
andresf2448 <andresf2448@gmail.com>
2023-12-19 18:23:33 -0500

    Clase 8

--- Clase 8/main.js
+++ Clase 8/main.js
@@ -73,23 +73,44 @@
 //dado un arreglo de premios, crear una función que genere un premio aleatorio
 
 //al ejecutar ambas funciones me debe mostrar aleatoriamente la persona y el premio que ha ganado
-const nombres = ["juanita", "pedrito", "andres", "carlitos", "marina"];
-const premios = ["celular", "carro", "ropa", "computador"];
+// const nombres = ["juanita", "pedrito", "andres", "carlitos", "marina"];
+// const premios = ["celular", "carro", "ropa", "computador"];
 
-const generarNombreAleatorio = () => {
-  let indice = Math.floor(Math.random() * nombres.length);
+// const generarNombreAleatorio = () => {
+//   let indice = Math.floor(Math.random() * nombres.length);
 
-  return nombres[indice];
-};
+//   return nombres[indice];
+// };
 
-const generarPremioAleatorio = () => {
-  let indice = Math.floor(Math.random() * premios.length);
+// const generarPremioAleatorio = () => {
+//   let indice = Math.floor(Math.random() * premios.length);
 
-  return premios[indice];
-};
+//   return premios[indice];
+// };
 
-for (let i = 0; i < 4; i++) {
-  alert(
-    `Ganador ${generarNombreAleatorio()} Premio: ${generarPremioAleatorio()}`
-  );
-}
+// for (let i = 0; i < 4; i++) {
+//   alert(
+//     `Ganador ${generarNombreAleatorio()} Premio: ${generarPremioAleatorio()}`
+//   );
+// }
+
+//DATE
+// console.log(Date());
+// console.log(new Date(2023, 11, 19, 10, 10, 10));
+
+// const navidad = new Date("December 24, 2023 23:59:59");
+// console.log(navidad);
+// console.log(navidad.toDateString());
+// console.log(navidad.toLocaleString());
+// console.log(navidad.toLocaleDateString());
+// console.log(navidad.toTimeString());
+// console.log(navidad.getFullYear());
+// console.log(navidad.getMonth());
+// console.log(navidad.getDay());
+
+const navidad = new Date("December 24, 2023");
+const hoy = new Date("December 19, 2023");
+console.log(navidad - hoy);
+
+const milisegundoPorDia = 86400000;
+console.log((navidad - hoy) / milisegundoPorDia);
