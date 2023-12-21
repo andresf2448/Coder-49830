818961602640a7f10520c443dd669856376911a4
andresf2448 <andresf2448@gmail.com>
2023-12-21 18:37:13 -0500

    Clase 9

--- Clase 9/main.js
+++ Clase 9/main.js
@@ -29,17 +29,65 @@
 // let saludo = document.getElementById("saludo");
 // saludo.innerHTML = "<h1>Hola coder</h1>";
 
+// let contenedor = document.getElementById("contenedor");
+
+// let seccion = prompt("Ingrese el nombre de la seccion");
+
+// if (seccion === "carrito") {
+//   contenedor.innerHTML = "<h1>Bienvenid@ al carrito</h1>";
+//   contenedor.className = "rojo";
+// } else if (seccion === "favoritos") {
+//   contenedor.innerHTML = "<h1>Bienvenid@ a los favoritos</h1>";
+//   contenedor.className = "azul";
+// } else {
+//   contenedor.innerHTML = "<h1>Bienvenid@ a nuestra super página</h1>";
+//   contenedor.className = "amarillo";
+// }
+
+//CREACIÓN DE ELEMENTOS
+//1 paso: Cración del nodo
+// let div = document.createElement("div");
+
+//2 paso agregado de contenido
+// div.innerHTML = "<h1>Hola a todos</h1>";
+
+//agregado de un padre
+// 1 forma en el body
+// document.body.append(div);
+
+//2 forma
+// let contenedor = document.getElementById("contenedor");
+// contenedor.append(div);
+
+//ELIMINAR NODOS
+// let saludo = document.getElementById("saludo");
+// saludo.remove();
+
+// let contenedor = document.getElementById("contenedor");
+// const usuarios = ["carmen", "carlitos", "juan", "pedrito", "andres"];
+
+// usuarios.forEach((usuario) => {
+//   let li = document.createElement("li");
+//   li.innerHTML = usuario;
+//   contenedor.append(li);
+// });
+
+const productos = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "gorra", precio: 750 },
+  { id: 3, nombre: "media", precio: 370 },
+  { id: 4, nombre: "zapato", precio: 290 },
+];
+
 let contenedor = document.getElementById("contenedor");
 
-let seccion = prompt("Ingrese el nombre de la seccion");
+productos.forEach((producto) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h2>Id: ${producto.id}</h2>
+    <p>Nombre: ${producto.nombre}</p>
+    <b>$${producto.precio}</b>
+  `;
 
-if (seccion === "carrito") {
-  contenedor.innerHTML = "<h1>Bienvenid@ al carrito</h1>";
-  contenedor.className = "rojo";
-} else if (seccion === "favoritos") {
-  contenedor.innerHTML = "<h1>Bienvenid@ a los favoritos</h1>";
-  contenedor.className = "azul";
-} else {
-  contenedor.innerHTML = "<h1>Bienvenid@ a nuestra super página</h1>";
-  contenedor.className = "amarillo";
-}
+  contenedor.append(div);
+});
